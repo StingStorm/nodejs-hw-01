1e8adc7d76cc0bef33d07b760095d5d541abf259
StingStorm <alexkalganov18@gmail.com>
2024-11-27 04:10:28 +0200

    add all tasks

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,5 @@
-export const PATH_DB =
+import path from 'node:path';
+
+const pathToWorkDir = process.cwd();
+
+export const PATH_DB = path.join(pathToWorkDir, 'src', 'db', 'db.json');
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,16 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    //or just writeContacts([]) ??
+    const contacts = await readContacts();
+    contacts.length = 0;
+
+    await writeContacts(contacts);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeAllContacts();
